6163908f33a312f1c289396727e67477c262ae7a
Dimitrios Dekas <dimitride@csd.auth.gr>
2023-07-20 11:29:27 +0300

    Add files via upload

---
+++ src/preprocess/io_handler.py
@@ -0,0 +1,132 @@
+'''
+    File name: io_handler.py
+    Author: Dekas Dimitrios
+    Date last modified: 07/08/2023
+    Python Version: 3.9
+'''
+
+
+import pandas as pd
+from tqdm import tqdm
+from sklearn.utils import shuffle
+from ekp import apply_ekphrasis_extra
+
+
+def load_training_tweets_from_text_file(path):
+    """
+    Given a system path that corresponds to a text file this function
+    is able to return a list of the tweets contained in the text file.
+
+    :param path: string representing a valid system path
+    :return: list of tweets contained in the text file provided as the function parameter.
+    """
+    tweets = []
+    with open(path, 'r', encoding='utf-8') as file:
+        for tweet in tqdm(file):
+            tweets.append(tweet.rstrip('\n'))
+    return tweets
+
+
+def load_testing_tweets_from_text_file(path):
+    """
+    Given a system path that corresponds to a text file this function
+    is able to return a list of the tweets contained in the text file.
+
+    It also takes into account the structure of the test file that includes
+    the index of each tweet, e.g. '1, Football is amazing #LoveTheGame'
+
+    :param path: string representing a valid system path
+    :return: list of tweets contained in the text file provided as the function parameter.
+    """
+    tweets = []
+    with open(path, 'r', encoding='utf-8') as file:
+        for tweet in tqdm(file):
+            tweets.append("".join(tweet.rstrip('\n').split(',')[1:]))
+    return tweets
+
+
+def load_training_tweets_to_df(pos_path, neg_path, seed):
+    """
+    Given two file paths and a random seed the function is able to return a dataframe that contains both the
+    positive and negative tweets in the dataset, also providing us with the appropriate labeling scheme.
+
+    :param pos_path: a string representing a valid system path corresponding to the file that contain the positive tweets
+    :param neg_path: a string representing a valid system path corresponding to the file that contain the negative tweets
+    :param seed: an integer representing the random seed that will control the shuffling of the dataset.
+    :return: a dataframe that contains the tweets in the training set
+    """
+    # Loading of the initial training tweets as provided to us
+    train_pos_tweets = load_training_tweets_from_text_file(pos_path)
+    train_pos_labels = [1] * len(train_pos_tweets)
+    train_neg_tweets = load_training_tweets_from_text_file(neg_path)
+    train_neg_labels = [-1] * len(train_neg_tweets)
+
+    # Unify the two types of training tweets
+    train_tweets = train_pos_tweets + train_neg_tweets
+    train_labels = train_pos_labels + train_neg_labels
+
+    # Shuffle the tweets
+    train_tweets, train_labels = shuffle(train_tweets, train_labels, random_state=seed)
+
+    # Use a pandas dataframe to store them for easier handling
+    train_tweet_df = pd.DataFrame({'tweet': train_tweets, 'label': train_labels})
+
+    return train_tweet_df
+
+
+def load_testing_tweets_to_df(file_path):
+    """
+    Given a file path the function is able to return a dataframe that contains the testing tweets in the dataset.
+
+    :param file_path: a string representing a valid system path corresponding to the file that contain the testing tweets
+    :return: a dataframe that contains the tweets in the test set
+    """
+    # Loading of the initial testing tweets as provided to us
+    test_tweets = load_testing_tweets_from_text_file(file_path)
+
+    # Use a pandas dataframe to store them for easier handling
+    test_tweets_df = pd.DataFrame({'tweet': test_tweets})
+
+    return test_tweets_df
+
+
+def write_tweets_to_text_file(tweets, path):
+    """
+    Given a list of tweets and a valid system path the function can be used to store
+    the tweets in a text file format inside the file specified by the system path.
+
+    :param tweets: a list containing the tweets to be saved
+    :param path: a system path that indicated the file in which the text file will be saved
+    """
+    with open(path, 'w') as file:
+        for tweet in tweets:
+            file.write(f"{tweet}\n")
+
+
+def load_extra_dataset(file_path, seed):
+    """
+    Given a file patha and a random seed the function is able to return a dataframe that contains both the
+    tweets in the extra dataset, also providing us with the appropriate labeling scheme.
+
+    :param file_path: a string representing a valid system path corresponding to the file that contain the extra dataset
+    :param seed: an integer representing the random seed that will control the shuffling of the dataset.
+    :return: a dataframe that contains the tweets in the training set
+    """
+    # Importing the dataset
+    df = pd.read_csv(file_path, encoding="ISO-8859-1", names=["sentiment", "ids", "date", "flag", "user", "text"])
+
+    # Removing the unnecessary columns.
+    df = df[['sentiment', 'text']]
+    df = df.rename(columns={'text': 'tweet', 'sentiment': 'label'})
+
+    # Replacing the values to ease understanding.
+    df['label'] = df['label'].replace(4, 1)
+    df['label'] = df['label'].replace(0, -1)
+
+    # Shuffle the dataset
+    shuffled_df = df.sample(frac=1, random_state=seed).reset_index(drop=True)
+
+    # Normalize users and urls using ekphrasis
+    shuffled_df = apply_ekphrasis_extra(shuffled_df)
+
+    return shuffled_df
---
+++ src/preprocess/word_level.py
@@ -0,0 +1,375 @@
+"""
+    File name: word_level.py
+    Author: Dekas Dimitrios
+    Date last modified: 07/08/2023
+    Python Version: 3.9
+"""
+
+
+import re
+import string
+import time
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from tqdm import tqdm
+
+tqdm.pandas()
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+stop_words = set(stopwords.words('english'))
+punctuations = string.punctuation
+lemmatizer = WordNetLemmatizer()
+
+
+# Hard coded spelling corrections derived by inspecting our data
+spell_dict = {"i'm": "i am",
+              "u": "you",
+              "don't": "do not",
+              "it's": "it is",
+              "its": "it is",
+              "im": "i am",
+              "i'll": "i will",
+              "that's": "that is",
+              "you're": "you are",
+              "dont": "do not",
+              "i've": "i have",
+              "didn't": "did not",
+              "ur": "you are",
+              "ill": "i will",
+              "won't": "will not",
+              "he's": "he is",
+              "thats": "that is",
+              "TRUE": "true",
+              "she's": "she is",
+              "haven't": "have not",
+              "doesn't": "does not",
+              "what's": "what is",
+              "i'd": "i would",
+              "you'll": "you will",
+              "there's": "there is",
+              "we're": "we are",
+              "isn't": "is not",
+              "let's": "let us",
+              "they're": "they are",
+              "wasn't": "was not",
+              "didnt": "did not",
+              "couldn't": "could not",
+              "you've": "you have",
+              "we'll": "we will"}
+
+
+def to_lower_case(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to only contain sentences with lowercase characters.
+
+    :param df: a dataframe that the lower-casing procedure is going to be applied to
+    """
+    df['to_lower'] = df['processed_tweet'].progress_apply(lambda x: x.lower())
+    df['processed_tweet'] = df['to_lower'].copy()
+
+
+def remove_user(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the <user> tokens from its sentences.
+
+    :param df: a dataframe that the removal of the <user> token is going to be applied to
+    """
+    df['remove_user'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if w != '<user>']))
+    df['processed_tweet'] = df['remove_user'].copy()
+
+
+def remove_url(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the <url> tokens from its sentences.
+
+    :param df: a dataframe that the removal of the <url> token is going to be applied to
+
+    """
+    df['remove_url'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if w != '<url>']))
+    df['processed_tweet'] = df['remove_url'].copy()
+
+
+def change_user(df, user_rep):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to change the <user> tokens from its sentences to an alternative
+    representation given as a function argument.
+
+    :param df: a dataframe that the change of the <user> token is going to be applied to
+    :param user_rep: a string representing the alternative representation used to transform the <user> token.
+    """
+    df['change_user'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([user_rep if w == '<user>' else w for w in x.split()]))
+    df['processed_tweet'] = df['change_user'].copy()
+
+
+def change_url(df, url_rep):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to change the <url> tokens from its sentences to an alternative
+    representation given as a function argument.
+
+    :param df: a dataframe that the change of the <url> token is going to be applied to
+    :param user_rep: a string representing the alternative representation used to transform the <url> token.
+    """
+    df['change_url'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([url_rep if w == '<url>' else w for w in x.split()]))
+    df['processed_tweet'] = df['change_url'].copy()
+
+
+def remove_elongated(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the <elongated> tokens from its sentences.
+
+    :param df: a dataframe that the removal of the <url> token is going to be applied to
+    """
+    df['remove_elongated'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w if w != '<elongated>' else ' ' for w in x.split()]))
+    df['processed_tweet'] = df['remove_elongated'].copy()
+
+
+def remove_stopwords(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the stopword tokens from its sentences.
+
+    :param df: a dataframe that the removal of stopwords token is going to be applied to
+    """
+    df['remove_stopwords'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if w.lower() not in stop_words]))
+    df['processed_tweet'] = df['remove_stopwords'].copy()
+
+
+def remove_punctuations(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the punctuation tokens from its sentences.
+
+    :param df: a dataframe that the removal of stopwords token is going to be applied to
+    """
+    df['remove_punctuations'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if w.lower() not in punctuations]))
+    df['processed_tweet'] = df['remove_punctuations'].copy()
+
+
+def remove_retweets(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the retweet tags from its sentences.
+
+    :param df: a dataframe that the removal of stopwords token is going to be applied to
+    """
+    df['remove_retweets'] = df['processed_tweet'].progress_apply(lambda x: re.sub(r'\brt\b', '', x))
+    df['processed_tweet'] = df['remove_retweets'].copy()
+
+
+def normalize_laughter(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to change the laughing patterns from its sentences to an alternative representation.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    laughter_pattern = re.compile(r'.*(h+a+h+a+|a+h+a+h+).*', re.IGNORECASE)
+    df['normalize_laughter'] = df['processed_tweet'].progress_apply(lambda x: ' '.join(['haha' if laughter_pattern.match(w) else w for w in x.split()]))
+    df['processed_tweet'] = df['normalize_laughter'].copy()
+
+
+def remove_single_characters(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to  remove single character words from its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    df['remove_single_characters'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if len(w) > 1]))
+    df['processed_tweet'] = df['remove_single_characters'].copy()
+
+
+def remove_angle_brackets(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove angle brackets surrounding words in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    df['remove_angle_brackets'] = df['processed_tweet'].progress_apply(lambda x: re.sub(r'<([^<>]+)>', r'\1', x))
+    df['processed_tweet'] = df['remove_angle_brackets'].copy()
+
+
+def lemmatize(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to lemmatize words in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    df['lemmatize'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([lemmatizer.lemmatize(w) for w in x.split()]))
+    df['processed_tweet'] = df['lemmatize'].copy()
+
+
+def normalize_repeating_characters(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to normalize repetitions of the same character into a single instance of the character.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    repetition_pattern = re.compile(r'([a-z])\1{3,}')
+    df['normalize_rep_chars'] = df['processed_tweet'].progress_apply(lambda x: repetition_pattern.sub(r'\1', x))
+    df['processed_tweet'] = df['normalize_rep_chars'].copy()
+
+
+def remove_numeric(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove words that include numeric values in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    digit_pattern = r'\d'
+    df['remove_numeric'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([w for w in x.split() if not re.search(digit_pattern, w)]))
+    df['processed_tweet'] = df['remove_numeric'].copy()
+
+
+def remove_extra_spaces(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove extra spaces found between words in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    df['remove_extra_spaces'] = df['processed_tweet'].progress_apply(lambda x: re.sub(r'\s+', ' ', x).strip())
+    df['processed_tweet'] = df['remove_extra_spaces'].copy()
+
+
+def remove_trailing_urls(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to remove the trailing '... url' instances in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    pattern = ' ... <url>'
+    df['remove_trailing_urls'] = df['processed_tweet'].progress_apply(lambda s: s[:-len(pattern)] if s.endswith(pattern) else s)
+    df['processed_tweet'] = df['remove_trailing_urls'].copy()
+
+
+def manual_spell_correction(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to manually correct spelling in its sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    df['spell_correction'] = df['processed_tweet'].progress_apply(lambda x: ' '.join([spell_dict[w] if w in spell_dict else w for w in x.split()]))
+    df['processed_tweet'] = df['spell_correction'].copy()
+
+
+def clarify_empty(df):
+    """
+    Function that when applied to a dataframe transforms its 'processed_tweet' column
+    to manually label its empty sentences.
+
+    :param df: a dataframe that the change of the laughing patterns is going to be applied to
+    """
+    empty_string_placeholder = "$!@EMPTYSTRING@!$"
+    df['clarify_empty'] = df['processed_tweet'].progress_apply(lambda x: x if x != '' and x != ' ' else empty_string_placeholder)
+    df['processed_tweet'] = df['clarify_empty'].copy()
+
+    mask = df['processed_tweet'] == empty_string_placeholder
+    df.loc[mask, 'processed_tweet'] = df.loc[mask, 'tweet']
+
+
+def apply_word_level(df, cfg):
+    """
+    Function that when applied to a dataframe transforms a 'tweet' column to
+    produce a new 'processed_tweet' column where the transformations designated
+    by the config are performed to produce its content.
+
+    :param df: a dataframe that the transformations are going to be applied to
+    :param cfg: a config that indicated the transformations to be applied to the df argument
+    :return: the dataframe obtained by applying the desired transformations
+    """
+    # Create a column named "processed_tweet" if no previous preprocessing step did so.
+    if 'processed_tweet' not in df.columns:
+        df['processed_tweet'] = df['tweet'].copy()
+
+    if cfg.LOWER_CASE:
+        print(">>> Applying lower casing")
+        time.sleep(2)  # Used for the prints and tqdm co-existence to not produce weird visual effects
+        to_lower_case(df)
+    if cfg.REMOVE_TRAILING_URL:
+        print(">>> Removing trailing urls")
+        time.sleep(2)
+        remove_trailing_urls(df)
+    if cfg.REMOVE_USER:
+        print(">>> Removing user tags")
+        time.sleep(2)
+        remove_user(df)
+    if cfg.CHANGE_USER:
+        print(">>> Changing user tags")
+        time.sleep(2)
+        change_user(df, cfg.USER_REPRESENTATION)
+    if cfg.REMOVE_URL:
+        print(">>> Removing url tags")
+        time.sleep(2)
+        remove_url(df)
+    if cfg.CHANGE_URL:
+        print(">>> Changing url tags")
+        time.sleep(2)
+        change_url(df, cfg.URL_REPRESENTATION)
+    if cfg.NORMALIZE_LAUGHTER:
+        print(">>> Normalizing laughter")
+        time.sleep(2)
+        normalize_laughter(df)
+    if cfg.REMOVE_ELONGATED:
+        print(">>> Removing elongated tags")
+        time.sleep(2)
+        remove_elongated(df)
+    if cfg.REMOVE_RETWEETS:
+        print(">>> Removing retweet tags")
+        time.sleep(2)
+        remove_retweets(df)
+    if cfg.REMOVE_SINGLE_CHARACTERS:
+        print(">>> Removing single characters")
+        time.sleep(2)
+        remove_single_characters(df)
+    if cfg.REMOVE_ANGLE_BRACKETS:
+        print(">>> Removing Angle Brackets")
+        time.sleep(2)
+        remove_angle_brackets(df)
+    if cfg.LEMMATIZE:
+        print(">>> Lemmatizing")
+        time.sleep(2)
+        lemmatize(df)
+    if cfg.NORMALIZE_REPEATING_CHARACTERS:
+        print(">>> Normalizing repeating characters")
+        time.sleep(2)
+        normalize_repeating_characters(df)
+    if cfg.REMOVE_NUMERIC:
+        print(">>> Removing numeric words")
+        time.sleep(2)
+        remove_numeric(df)
+    if cfg.REMOVE_EXTRA_SPACES:
+        print(">>> Removing extra spaces")
+        time.sleep(2)
+        remove_extra_spaces(df)
+    if cfg.MANUAL_SPELL_CORRECTION:
+        print(">>> Manual spell correction")
+        time.sleep(2)
+        manual_spell_correction(df)
+    if cfg.REMOVE_STOPWORDS:
+        print(">>> Removing stopwords")
+        time.sleep(2)
+        remove_stopwords(df)
+    if cfg.REMOVE_PUNCTUATIONS:
+        print(">>> Removing punctuations")
+        time.sleep(2)
+        remove_punctuations(df)
+    if cfg.CLARIFY_EMPTY:
+        print('>>> Clarifying empty tweets')
+        time.sleep(2)
+        clarify_empty(df)
+    return df
